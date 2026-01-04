1204350bd331597c68158000930794b147997d3c
DarekDGB <majescyk@gmail.com>
2026-01-04 11:28:43 +0000

    feat(v3): harden Sentinel contract parsing, hashing, and reason codes

--- src/sentinel_ai_v2/contracts/__init__.py
+++ src/sentinel_ai_v2/contracts/__init__.py
@@ -1,1 +1,11 @@
-todo
+from .v3_types import SentinelV3Request, SentinelV3Response, SentinelV3Constraints
+from .v3_reason_codes import ReasonCode
+from .v3_hash import canonical_sha256
+
+__all__ = [
+    "SentinelV3Request",
+    "SentinelV3Response",
+    "SentinelV3Constraints",
+    "ReasonCode",
+    "canonical_sha256",
+]
